feacef23606d1648cad3acb49b8757dd79980a77
Gavin Asay <gavin.asay@gmail.com>
2021-04-08 04:23:18 -0600

    client code

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 
@@ -6,15 +7,25 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const { authMiddleware } = require('./utils/auth');
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
+	context: authMiddleware,
 });
 
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
 	app.listen(PORT, () => {
