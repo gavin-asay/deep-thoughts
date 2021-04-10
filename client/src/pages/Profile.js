73fe55b6fea02a099f89d6796ea8cdf3fab1d187
Gavin Asay <gavin.asay@gmail.com>
2021-04-10 03:19:31 -0600

    reaction and thought forms

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
-import { useQuery } from '@apollo/react-hooks';
+import ThoughtForm from '../components/ThoughtForm';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const Profile = () => {
@@ -12,6 +14,8 @@ const Profile = () => {
 	const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
 		variables: { username: userParam },
 	});
+
+	const [addFriend] = useMutation(ADD_FRIEND);
 
 	const user = data?.me || data?.user || {};
 
@@ -23,12 +27,27 @@ const Profile = () => {
 		return <h4>You need to be logged in to see this page. Use the navigation links above to sign up or log in!</h4>;
 	}
 
+	const handleClick = async () => {
+		try {
+			await addFriend({
+				variables: { id: user._id },
+			});
+		} catch (e) {
+			console.error(e);
+		}
+	};
+
 	return (
 		<div>
 			<div className='flex-row mb-3'>
 				<h2 className='bg-dark text-secondary p-3 display-inline-block'>
 					Viewing {userParam ? `${user.username}'s` : 'your'} profile.
 				</h2>
+				{userParam && (
+					<button className='btn ml-auto' onClick={handleClick}>
+						Add Friend
+					</button>
+				)}
 			</div>
 
 			<div className='flex-row justify-space-between mb-3'>
@@ -40,6 +59,7 @@ const Profile = () => {
 					<FriendList username={user.username} friendCount={user.friendCount} friends={user.friends} />
 				</div>
 			</div>
+			<div className='mb-3'>{!userParam && <ThoughtForm />}</div>
 		</div>
 	);
 };
